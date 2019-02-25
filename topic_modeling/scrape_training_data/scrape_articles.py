adc20b342e563e489c0f006edf01416df005b930
Emma Templeton <emtempleton@gmail.com>
2019-02-25 14:03:44 -0500

    made scrape_articles.py PEP8 compliant

--- topic_modeling/scrape_training_data/scrape_articles.py
+++ topic_modeling/scrape_training_data/scrape_articles.py
@@ -2,17 +2,11 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-import re
-import string
-import glob
-import os
-import urllib.request
 from time import sleep
 from random import randint
-from IPython.core.display import clear_output
-from time import time
 import os.path
 import pandas as pd
+
 
 def simple_get(url):
     """
@@ -41,6 +35,7 @@ def is_good_response(resp):
             and content_type is not None 
             and content_type.find('html') > -1)
 
+
 def log_error(e):
     """
     It is always a good idea to log errors. 
@@ -49,32 +44,29 @@ def log_error(e):
     """
     print(e)
 
-    
-
 base_dir = '/dartfs-hpc/rc/home/5/f002s75/web_scraping'
-links = pd.read_csv(os.path.join(base_dir,'article_links.txt'), header=None)
+links = pd.read_csv(os.path.join(base_dir, 'article_links.txt'), header=None)
 links = links[0]
 
 for link in links:
 
     paragraphs = []
-    url = link
-    raw_html = simple_get(url)
+    raw_html = simple_get(link)
 
-    title = url.split('/')[-1]
+    title = link.split('/')[-1]
     
-    if os.path.exists(os.path.join(base_dir,'the_dartmouth','{0}.txt'.format(title))) == False:
+    if not os.path.exists(os.path.join(base_dir, 'the_dartmouth', '{0}.txt'.format(title))):
 
         try:
 
             html = BeautifulSoup(raw_html, 'html.parser')
-            html = html.findAll('p',attrs={'class': None})
+            html = html.findAll('p', attrs={'class': None})
 
             for i, p in enumerate(html):
                 result = (i, p.text)
                 paragraphs.append(p.text)
 
-            paragraphs = paragraphs[2:] # this is a crappy way to get rid of the article info, but seems consistent
+            paragraphs = paragraphs[2:]  # this is a crappy way to get rid of the article info, but seems consistent
             s = ''.join(paragraphs)
             sep = '\n'
             s = s.split(sep, 1)[0]
@@ -83,15 +75,15 @@ for link in links:
 
                 short_title = title[0:199]
 
-                with open(os.path.join(base_dir,'the_dartmouth','{0}.txt'.format(short_title)), "wb") as text_file:
+                with open(os.path.join(base_dir, 'the_dartmouth', '{0}.txt'.format(short_title)), "wb") as text_file:
                         text_file.write(s.encode('utf8'))
             else:
 
-                with open(os.path.join(base_dir,'the_dartmouth','{0}.txt'.format(title)), "wb") as text_file:
+                with open(os.path.join(base_dir, 'the_dartmouth', '{0}.txt'.format(title)), "wb") as text_file:
                         text_file.write(s.encode('utf8'))
 
             # Pause the loop
-            sleep(randint(8,15))
+            sleep(randint(8, 15))
 
         except TypeError:
             pass
