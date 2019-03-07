57028c72dab0965c9713930c3fcff4625e314488
Emma Templeton <emtempleton@gmail.com>
2019-03-06 20:36:06 -0500

    added all code needed to validate models, compare them to each other, and then visually inspect the output.

--- topic_modeling/validate_models/validate_models.py
+++ topic_modeling/validate_models/validate_models.py
@@ -4,6 +4,8 @@ import glob
 import re
 import nltk
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 nltk.download('stopwords')
@@ -24,62 +26,138 @@ def preprocess_documents(doc, stemming=True):
     return s
 
 
-# need two different ways to set directory to pass
-# Travis CI (because .travis file is called from
-# different directory)
-current_file = os.getcwd().split('/')[-1]
+def apply_topic_models():
+    # need two different ways to set directory to pass
+    # Travis CI (because .travis file is called from
+    # different directory)
+    current_file = os.getcwd().split('/')[-1]
 
-if current_file == 'validate_models':
-    base_dir = os.getcwd()
-    model_dir = os.path.join(
-        os.path.normpath(os.getcwd() + os.sep + os.pardir),
-        'train_models', 'models_pickles')
-else:
-    base_dir = os.path.join(
-            os.getcwd(), 'topic_modeling', 'validate_models')
-    model_dir = os.path.join(
-        os.getcwd(), 'topic_modeling', 'train_models', 'models_pickles')
+    if current_file == 'validate_models':
+        base_dir = os.getcwd()
+        model_dir = os.path.join(
+            os.path.normpath(os.getcwd() + os.sep + os.pardir),
+            'train_models', 'models_pickles')
+    else:
+        base_dir = os.path.join(
+                os.getcwd(), 'topic_modeling', 'validate_models')
+        model_dir = os.path.join(
+            os.getcwd(), 'topic_modeling', 'train_models', 'models_pickles')
 
-model_list = glob.glob(os.path.join(model_dir, '*ing.pkl'))
+    model_list = glob.glob(os.path.join(model_dir, '*ing.pkl'))
 
-for topic_model in model_list:
+    for topic_model in model_list:
 
-    model_name = topic_model.split('/')[-1].split('.pkl')[0]
-    model_tf_name = '{}_tf.pkl'.format(model_name)
+        model_name = topic_model.split('/')[-1].split('.pkl')[0]
+        model_tf_name = '{}_tf.pkl'.format(model_name)
 
-    with open(topic_model, 'rb') as pickle_file:
-        model = pickle.load(pickle_file)
+        with open(topic_model, 'rb') as pickle_file:
+            model = pickle.load(pickle_file)
 
-    with open(os.path.join(model_dir, model_tf_name), 'rb') as pickle_file_tf:
-        tf_vectorizer = pickle.load(pickle_file_tf)
+        with open(os.path.join(model_dir, model_tf_name), 'rb') as pickle_file_tf:
+            tf_vectorizer = pickle.load(pickle_file_tf)
 
-    flist = glob.glob(os.path.join(base_dir, 'validation_data', '*.txt'))
+        flist = glob.glob(os.path.join(base_dir, 'validation_data', '*.txt'))
 
-    one_convo = []
-    topics = []
+        one_convo = []
+        topics = []
+
+        for document in flist:
+
+            # get file name
+            topic = document.split('/')[-1].split('_')[0]
+
+            data = pd.read_table(document)
+            data.columns = ['transcript']
+            one_convo.append(preprocess_documents(
+                            ",".join(data['transcript'].values)))
+            topics.append(topic)
+
+            # start with first element
+            topic_matrix = model.transform(tf_vectorizer.transform([one_convo[0]]))
+            topic_matrix = pd.DataFrame(topic_matrix)
+
+            for turn in one_convo[1:]:  # skip first one
+                topic_weights = model.transform(tf_vectorizer.transform([turn]))
+                topic_weights = pd.DataFrame(topic_weights)
+                topic_matrix = topic_matrix.append(topic_weights,
+                                                   ignore_index=True)
+
+        topic_matrix['topic'] = topics
+
+        topic_matrix.to_csv(os.path.join(base_dir, 'topic_vectors',
+                                         '{0}.csv'.format(model_name)))
+
+
+def flatten_topic_vectors():
+    model_list = glob.glob(os.path.join(base_dir,'topic_vectors','*.csv'))
+
+    for model in model_list:
+
+        data = pd.read_csv(model)
+        name = model.split('/')[-1].split('.csv')[0]
+        new_header = ['computer_science_1','computer_science_2','computer_science_3','computer_science_4','football_1','football_2','football_3','football_4','fraternities_and_sororities_1','fraternities_and_sororities_2','fraternities_and_sororities_3','fraternities_and_sororities_4','neuroscience_1','neuroscience_2','neuroscience_3','neuroscience_4','religion_1','religion_2','religion_3','religion_4','theatre_1','theatre_2','theatre_3','theatre_4']
+
+        data = data.sort_values(by=['topic'])
+        data.pop('Unnamed: 0')
+        data.pop('topic')
+
+        topics_transpose = data.transpose()
+        topics_transpose.columns = new_header
+
+        corr_matrix=topics_transpose.corr()
+
+        data_flat = corr_matrix.values.flatten()
+        np.save(os.path.join(base_dir, 'topic_vectors_flat', '{0}').format(name), data_flat)
+
+
+# Compare to an 'ideal' version
+# Probably a better way to generate the ideal version in the first place
+
+def compare_to_perfect_model_performance():
+    perfect_model = pd.read_csv(os.path.join(base_dir, 'perfect_model.csv'), header=None)
+    perfect_model = perfect_model.values.flatten()
+
+    flist = glob.glob(os.path.join(base_dir, 'topic_vectors_flat', '*.npy'))
+
+    evaluate_wiki = pd.DataFrame()
+    evaluate_wiki = evaluate_wiki.fillna(0)  # with 0s rather than NaNs
+
+    counter = 0
 
     for file in flist:
+        test_data = np.load(file)
 
-        # get file name
-        topic = file.split('/')[-1].split('_')[0]
+        name = file.split('/')[-1].split('.npy')[0]
 
-        data = pd.read_table(file)
-        data.columns = ['transcript']
-        one_convo.append(preprocess_documents(
-                        ",".join(data['transcript'].values)))
-        topics.append(topic)
+        correlation_term = np.corrcoef(test_data, perfect_model)[0][1]
 
-        # start with first element
-        topic_matrix = model.transform(tf_vectorizer.transform([one_convo[0]]))
-        topic_matrix = pd.DataFrame(topic_matrix)
+        evaluate_wiki.at[counter, 'model'] = name
+        evaluate_wiki.at[counter, 'correlation'] = correlation_term
 
-        for turn in one_convo[1:]:  # skip first one
-            topic_weights = model.transform(tf_vectorizer.transform([turn]))
-            topic_weights = pd.DataFrame(topic_weights)
-            topic_matrix = topic_matrix.append(topic_weights,
-                                               ignore_index=True)
+        counter = counter + 1
 
-    topic_matrix['topic'] = topics
 
-    topic_matrix.to_csv(os.path.join(base_dir, 'topic_vectors',
-                                     '{0}.csv'.format(model_name)))
+def plot_model_comparisions():
+    plt.style.use('seaborn-white')
+    plt.figure(figsize=(20,10))
+    plt.bar(evaluate_wiki['model'],evaluate_wiki['correlation'])
+    plt.margins(x=0.005)
+    plt.xticks(rotation='vertical', fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.xlabel('Model', fontsize=20)
+    plt.ylabel('correlation with a perfect fit', fontsize=20)
+    plt.grid(axis='y')
+
+    axes = plt.gca()
+    axes.set_ylim([0,1])
+
+    plt.tight_layout()
+    plt.savefig('model_evaluation.png', edgecolor='none', dpi=300)
+
+
+# TODO
+# 1. make functions work together (give parameters that they need to run)
+# 2. run from command line by adding __main__
+# 3. train many different models in train_models.py (need to figure out argparsing)
+# 4. worst case, solve by how i solved the nbackseq
+
